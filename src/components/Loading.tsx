7d8ecd1139bacc13e4f096ec37ea2aa1f2acc87b
davhyandrade <davhyhello@gmail.com>
2024-09-14 22:20:52 -0300

    feat: add loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import SpinnerIcon from '../assets/icons/SpinnerIcon';
+
+interface ILoadingProps {
+  name?: string;
+}
+
+const Loading = ({ name }: ILoadingProps) => {
+  const [verifiedName, setVerifiedName] = useState<string | undefined>(name);
+
+  const checkLoadingName = (name: string) => {
+    let startIndex = name.length - 3;
+    let endIndex = name.length;
+
+    if (name.substring(startIndex, endIndex) === '...') return console.log('tem');
+
+    setVerifiedName(name.concat('...'));
+  };
+
+  useEffect(() => {
+    if (name) checkLoadingName(name);
+  }, []);
+
+  return (
+    <div id="loading-field">
+      <div>
+        <SpinnerIcon />
+        {name && <span>{verifiedName}</span>}
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
